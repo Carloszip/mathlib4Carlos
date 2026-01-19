0a3fd31e660f05aed87f31f80be18dbb239d6a11
Carlos.zip <cgb25052013@gmail.com>
2026-01-19 23:32:57 +0100

    More bullshit

--- Mathlib/LinearAlgebra/Matrix/SylvesterCrit.lean
+++ Mathlib/LinearAlgebra/Matrix/SylvesterCrit.lean
@@ -52,14 +52,39 @@ theorem Pos_Det_leadingMinors_if_isPosDef {M : Matrix (Fin n) (Fin n) R}
       --leading minor is hermitian
       --because M.leadingMinor i j = M i j = star (M j i) = star (M.leadingMinor j i)
     · intro x hx
+      let x2sup : Finset (Fin n) := {
+        val := x.support.val.map (fun j => finCast i j)
+        nodup := by
+          refine Multiset.Nodup.map ?_ ?_
+          · exact injective_of_le_imp_le (fun j ↦ finCast i j) fun {x y} a ↦ a
+          · exact x.support.nodup
+        }
+      let x2fun : Fin n → R := fun (k : Fin n) =>
+          if isLt : k.val < i then
+            x ⟨k.val, isLt⟩
+          else
+            0 -- x2 k = x k if k ≤ i, 0 if not
       let x2: Fin n →₀ R :=
-        { support := sorry --I dont know what this is (something to do with x.support)
-          toFun := fun k =>
-        if isLt : k.val < i then
-          x ⟨k.val, isLt⟩
-        else
-          0 -- x2 k = x k if k ≤ i, 0 if not
-          mem_support_toFun := sorry } --I also dont know what this is
+        { support := x2sup
+          toFun := x2fun
+          mem_support_toFun := by
+            let h := x.mem_support_toFun
+            intro a
+            by_cases H: a.val < i
+            · let a': Fin i := {
+                val := a.val
+                isLt := by exact H
+              }
+              have h1 : a ∈ x2sup ↔ a' ∈ x.support := by sorry
+              have h2 : (x2fun a ≠ 0) ↔ (x.toFun a' ≠ 0) := by sorry
+              specialize h a'
+              rw [h1,h2]
+              exact h
+            · push_neg at H
+              have h1 : (a ∉ x2sup) := by sorry
+              have h2 : x2fun a  = 0 := by sorry
+              exact (iff_false_left h1).mpr fun a ↦ a h2
+        }
       have hx2 : x2 ≠ 0 := by
         refine Finsupp.ne_iff.mpr ?_
         rw [Finsupp.ne_iff] at hx
@@ -69,7 +94,30 @@ theorem Pos_Det_leadingMinors_if_isPosDef {M : Matrix (Fin n) (Fin n) R}
         rw[this]
         exact ha
       have : (x.sum fun j xj ↦ x.sum fun k xk ↦ star xj * M.leadingMinor i j k * xk) =
-        (x2.sum fun j xj ↦ x2.sum fun k xk ↦ star xj * M j k * xk) := by sorry
+        (x2.sum fun j xj ↦ x2.sum fun k xk ↦ star xj * M j k * xk) := by
+          nth_rw 2 [show x.sum = fun g ↦ ∑ a ∈ x.support, g a (x a) from rfl]
+          rw [show
+              (x.sum fun j xj ↦
+                  (fun g ↦ ∑ a ∈ x.support, g a (x a)) fun k xk ↦
+                    star xj * M.leadingMinor i j k * xk) =
+                ∑ a ∈ x.support,
+                  (fun j xj ↦
+                      (fun g ↦ ∑ a ∈ x.support, g a (x a)) fun k xk ↦
+                        star xj * M.leadingMinor i j k * xk)
+                    a (x a)
+              from rfl]
+          nth_rw 2 [show x2.sum = fun g ↦ ∑ a ∈ x2.support, g a (x2 a) from rfl]
+          rw [show
+              (x2.sum fun j xj ↦
+                  (fun g ↦ ∑ a ∈ x2.support, g a (x2 a)) fun k xk ↦
+                    star xj * M j k * xk) =
+                ∑ a ∈ x2.support,
+                  (fun j xj ↦
+                      (fun g ↦ ∑ a ∈ x2.support, g a (x2 a)) fun k xk ↦
+                        star xj * M j k * xk)
+                    a (x2 a)
+              from rfl]
+          sorry --this is by def of x2.support
         --these definitions are always weird
       rw[this]
       exact hM.2 hx2
