078c57310d39706339d948de07a278179fb963de
Carlos.zip <cgb25052013@gmail.com>
2026-01-24 19:15:02 +0100

    Many more things

--- Mathlib/LinearAlgebra/Matrix/SylvesterCrit.lean
+++ Mathlib/LinearAlgebra/Matrix/SylvesterCrit.lean
@@ -34,35 +34,31 @@ abbrev R := ℝ --we need RClike for posdef implies pos determinant, and partial
 variable {A : Matrix (Fin n) (Fin n) R}
 
 -- this is probably defined somewhere else
-def finCast (i : Fin n) : Fin (i + 1) → Fin n :=
-fun k => ⟨k.val, Nat.lt_of_lt_of_le k.is_lt (Nat.succ_le_of_lt i.is_lt)⟩
+def finCast {i : ℕ} (hi : i ≤ n) : Fin i → Fin n :=
+fun k => ⟨k.val, Fin.val_lt_of_le k hi ⟩
 
 -- I wasnt able to define this without losing generality, so now n is just a natural number
-def leadingMinor (A : Matrix (Fin n) (Fin n) R) (i : Fin n) :
-  Matrix (Fin (i + 1)) (Fin (i + 1)) R := A.submatrix (finCast i) (finCast i)
-
-theorem isPosDef_if_Det_pos_leadingMinors {M : Matrix (Fin n) (Fin n) R}
-(h : ∀ i : Fin n , (M.leadingMinor i).det > 0) : M.PosDef := by sorry
+def leadingMinor (A : Matrix (Fin n) (Fin n) R) (i : ℕ) (hi : i ≤ n) :
+  Matrix (Fin i) (Fin i) R := A.submatrix (finCast hi) (finCast hi)
 
 lemma PosDef_leadingMinor_if_isPosDef {M : Matrix (Fin n) (Fin n) R}
-(h : M.IsHermitian) (hM : M.PosDef) (i : Fin n) : (M.leadingMinor i).PosDef := by
+(h : M.IsHermitian) (hM : M.PosDef) (i : ℕ) (hi : i ≤ n) : (M.leadingMinor i hi).PosDef := by
   unfold PosDef
   constructor -- it is hermitian and blablabla
   · ext j k -- easy part [done]
-    exact IsHermitian.apply h ⟨↑j, finCast._proof_1 i j⟩ ⟨↑k, finCast._proof_1 i k⟩
+    exact IsHermitian.apply h ?_ ?_
     -- leading minor is hermitian
     -- because M.leadingMinor i j = M i j = star (M j i) = star (M.leadingMinor j i)
   · intro x hx -- not so easy part [not_done]
     -- we define x2 as an extension of x
     let x2sup : Finset (Fin n) := { -- we define the support
-      val := x.support.val.map (fun j => finCast i j)
+      val := x.support.val.map (fun j => finCast hi j)
       nodup := by
-        refine Multiset.Nodup.map ?_ ?_
-        · exact injective_of_le_imp_le (fun j ↦ finCast i j) fun {x y} a ↦ a
-        · exact x.support.nodup
+        have hinj := injective_of_le_imp_le (fun j ↦ finCast hi j) fun {x y} a ↦ a
+        exact Multiset.Nodup.map hinj x.support.nodup
       }
     let x2fun : Fin n → R := fun (k : Fin n) => -- the function
-        if isLt : k.val < i + 1 then
+        if isLt : k.val < i then
           x ⟨k.val, isLt⟩
         else
           0 -- x2 k = x k if k ≤ i, 0 if not
@@ -72,9 +68,10 @@ lemma PosDef_leadingMinor_if_isPosDef {M : Matrix (Fin n) (Fin n) R}
         mem_support_toFun := by -- hideous junk
           let h := x.mem_support_toFun
           intro a
-          by_cases H: a.val < i + 1 -- if a is less than i then it comes from the original x,
+          by_cases H: a.val < i
+          -- if a is less than i then it comes from the original x,
           -- else it is 0
-          · let a': Fin (i + 1) := {
+          · let a': Fin i := {
               val := a.val
               isLt := by exact H
             }
@@ -94,9 +91,8 @@ lemma PosDef_leadingMinor_if_isPosDef {M : Matrix (Fin n) (Fin n) R}
                 unfold x2sup
                 simp only [Multiset.mem_map, mem_val, Finsupp.mem_support_iff, ne_eq]
                 use a'
-                constructor
-                · exact Finsupp.mem_support_iff.mp h
-                · exact rfl
+                exact ⟨Finsupp.mem_support_iff.mp h ,
+                rfl ⟩
             have h2 : (x2fun a ≠ 0) ↔ (x.toFun a' ≠ 0) := by -- this is clear enough [done]
               unfold x2fun
               rw [dif_pos H]
@@ -106,14 +102,12 @@ lemma PosDef_leadingMinor_if_isPosDef {M : Matrix (Fin n) (Fin n) R}
             exact h
           · push_neg at H -- this probably could be clearer [not_done]
             have h1 : (a ∉ x2sup) := by
-              simp only [mem_mk, Multiset.mem_map, mem_val, Finsupp.mem_support_iff, ne_eq,
-                not_exists, not_and, x2sup]
-              intro j hj
-              push_neg
-              have : (finCast i j).val < i.val + 1 := by
-                simp [finCast]
-              grw[H] at this
-              exact Ne.symm (Fin.ne_of_gt this)
+              intro ha
+              simp only [x2sup, Finset.mem_def, Multiset.mem_map] at ha
+              obtain ⟨j, hj, heq⟩ := ha
+              have heq': j.val = a.val := by exact Fin.mk.inj_iff.mp heq
+              have : a.val < i := by rw[←heq'] ; exact j.is_lt
+              linarith
             have h2 : x2fun a  = 0 := by
               simp only [dite_eq_right_iff, x2fun]
               intro h
@@ -124,19 +118,18 @@ lemma PosDef_leadingMinor_if_isPosDef {M : Matrix (Fin n) (Fin n) R}
       refine Finsupp.ne_iff.mpr ?_
       rw [Finsupp.ne_iff] at hx
       obtain ⟨a, ha⟩ := hx
-      use finCast i a
-      rw [show x2 (finCast i a) = x a from dif_pos a.isLt]
+      use finCast hi a
+      rw [show x2 (finCast hi a) = x a from dif_pos a.isLt]
       exact ha
-    -- the next part is really confusing (sorry :/ )
+    -- the next part is really confusing (sorry :/ ) [not_done]
     -- this is by def of x2.support
-    have : (x.sum fun j xj ↦ x.sum fun k xk ↦ star xj * M.leadingMinor i j k * xk) =
+    have : (x.sum fun j xj ↦ x.sum fun k xk ↦ star xj * M.leadingMinor i hi j k * xk) =
       (x2.sum fun j xj ↦ x2.sum fun k xk ↦ star xj * M j k * xk) := by
         unfold leadingMinor
         simp only [Matrix.submatrix_apply]
-        let f : Fin (i+1) ↪ Fin n :=
-          { toFun := finCast i -- this should probably be outside
-            inj' := by exact
-            injective_of_le_imp_le (finCast i) fun {x y} a ↦ a
+        let f : Fin i ↪ Fin n :=  -- this should probably be outside
+          { toFun := finCast hi
+            inj' := injective_of_le_imp_le (finCast hi) fun {x y} a ↦ a
           }
         have h_supp : x2.support = x.support.map f := rfl
         unfold Finsupp.sum --now is just a sum :)
@@ -147,32 +140,71 @@ lemma PosDef_leadingMinor_if_isPosDef {M : Matrix (Fin n) (Fin n) R}
         ext j
         congr
         ext k
-        by_cases H1 : f k < i.val + 1
+        by_cases H1 : f k < i
         · rw [dif_pos H1]
-          by_cases H2 : f j < i.val + 1
+          by_cases H2 : f j < i
           · rw [dif_pos H2]
             exact rfl
           · push_neg at H2 -- contradiction
-            have h1 : i.val + 1 ≤ (f j).val := H2
+            have h1 : i ≤ (f j).val := H2
             have h2 : (f j).val = j.val := rfl
-            have h3 : j.val < i.val + 1 := j.isLt
+            have h3 : j.val < i := j.isLt
             linarith
         · push_neg at H1 -- contradiction
-          have h1 : i.val + 1 ≤ (f k).val := H1
+          have h1 : i ≤ (f k).val := H1
           have h2 : (f k).val = k.val := rfl
-          have h3 : k.val < i.val + 1 := k.isLt
+          have h3 : k.val < i := k.isLt
           linarith
     rw[this]
     exact hM.2 hx2
 
 open scoped Classical in -- [done]
 theorem Det_pos_leadingMinors_if_isPosDef {M : Matrix (Fin n) (Fin n) R}
-(h : M.IsHermitian) (hM : M.PosDef) : ∀ i : Fin n , (M.leadingMinor i).det > 0 := by
-  exact fun i => (PosDef_leadingMinor_if_isPosDef h hM i).det_pos -- posdef matrices have pos. det.
+(h : M.IsHermitian) (hM : M.PosDef) :
+∀ i : Fin (n+1) , (M.leadingMinor i (Fin.is_le i)).det > 0 := by
+  exact fun i => (PosDef_leadingMinor_if_isPosDef h hM i (Fin.is_le i)).det_pos
+
+theorem isPosDef_if_Det_pos_leadingMinors {M : Matrix (Fin n) (Fin n) R}
+(h : ∀ i : Fin (n+1) , (M.leadingMinor i (Fin.is_le i)).det > 0) : M.PosDef := by
+  induction n with
+  | zero => -- kinda bloated but [done]
+      constructor
+      · exact isHermitian_iff_isSymmetric.mpr fun x ↦ congrFun rfl
+      · intro x hx
+        have : x = 0 := by
+          ext i
+          exact Fin.elim0 i
+        exact (hx this).elim
+  | succ n ih => -- (._.) [not_done]
+      let Mn := M.leadingMinor n (Nat.le_add_right n 1) -- block decomposition
+      have hMn_pos : Mn.PosDef := by -- Mn is posdef
+        apply ih
+        intro i
+        have heq : (Mn.leadingMinor i (Fin.is_le i)) = (M.leadingMinor i (Fin.is_le')) := by
+          ext i j
+          unfold Mn leadingMinor finCast
+          simp only [submatrix_apply]
+        rw [heq]
+        exact h ⟨↑i, Nat.lt_add_right 1 (Fin.is_lt i)⟩
+      have hM_Det_pos : M.det > 0 := by -- det M is > 0
+        have : M = M.leadingMinor (n+1) (Nat.le_refl (n + 1)) := rfl
+        rw [this]
+        exact h ⟨n+1, lt_add_one (n + 1)⟩
+      have hMn_Det_pos : Mn.det > 0 := by -- det Mn is > 0
+        have : Mn = M.leadingMinor (n) (Nat.le_add_right n 1) := rfl
+        rw [this]
+        exact h ⟨n, by refine Nat.lt_add_right 1 (lt_add_one n)⟩
+      /- sketch
+       / Mn v \ this is our matrix
+       \ vt d /
+
+       by the determinant formula we obtain what we should.
+      -/
+      sorry
 
 theorem isPosDef_iff_Det_pos_leadingMinors {M : Matrix (Fin n) (Fin n) R} -- [done]
-{h : M.IsHermitian} : M.PosDef ↔ (∀ i : Fin n , (M.leadingMinor i).det > 0) := by
-  exact ⟨ fun h1 i => Det_pos_leadingMinors_if_isPosDef h h1 i,
+{h : M.IsHermitian} : M.PosDef ↔ (∀ i : Fin (n+1) , (M.leadingMinor i (Fin.is_le i)).det > 0) := by
+  exact ⟨fun h1 i => Det_pos_leadingMinors_if_isPosDef h h1 i,
   fun h2 => isPosDef_if_Det_pos_leadingMinors h2⟩
 
 end Matrix
